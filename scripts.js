7131212a52b97183da690e37eda80228b6b65393
Yen <iknepper@hotmail.com>
2019-01-21 03:10:49 -0500

    maintenance

--- scripts.js
+++ scripts.js
@@ -1,16 +1,6 @@
 $(document).ready(function() {
-  console.log('ready!');
   getAlbumsByUserId(1, 'left');
   getAlbumsByUserId(2, 'right');
-
-  ////////////////// change selected to red
-  $('body').on('click', '.album__row', function() {
-    if (this.className.indexOf('selected') < 0) dragArr.add(this.id);
-    else dragArr.delete(this.id);
-
-    $(this).toggleClass('selected');
-    console.log(dragArr);
-  });
 
   //Live Album Filter
   $('.filter').keyup(function() {
@@ -32,16 +22,21 @@ $(document).ready(function() {
   });
 
   $('#user-submit').click(() => {
-    var value = $('#user-submit')
-      .siblings('input')
-      .val();
+    const value = $('#user-submit').siblings('input').val();
     if (value < 2 || value > 10) {
-      alert('User Id must be between 2 and 10');
-      // throw new Error('User Id must be between 2 and 10');
+        alert('User Id must be between 2 and 10');
     } else {
       getUserById(value);
       getAlbumsByUserId(value, 'right');
     }
+  });
+   //Highlight selected albums
+   $('body').on('click', '.album__row', function() {
+    if (!$(this).hasClass('selected')) multiSelect.add(this.id);
+    else multiSelect.delete(this.id);
+
+    $(this).toggleClass('selected');
+    // console.log(multiSelect);
   });
 });
 
@@ -49,10 +44,7 @@ const getUserById = async userId => {
   const url = 'https://jsonplaceholder.typicode.com/users/';
 
   try {
-    const user = await $.get(`${url}${userId}?`, data => {
-      data;
-    });
-    console.log(user.name);
+    const user = await $.get(`${url}${userId}?`, data => data);
     $('#their-name').text(
       `${user.name.split(' ')[user.name.split(' ').length - 2]}'s Albums`
     );
@@ -60,23 +52,22 @@ const getUserById = async userId => {
     return error;
   }
 };
+
 const getAlbumsByUserId = async (userId, direction) => {
   const url = 'https://jsonplaceholder.typicode.com/albums';
   const whichTable = direction === 'left' ? '#table-1' : '#table-2';
 
   try {
-    $(whichTable)
-      .children('.album__row')
-      .remove();
+    $(whichTable).children('.album__row').remove();
     const albums = await $.get(`${url}?userId=${userId}`);
 
     albums.map(album => {
       $(whichTable).append(`
-        <div class='table__row album__row' id="${
-          album.id
-        }" data-value="${album}" draggable="true" ondragstart="drag(event)" >
-        <div class='table__cell table__cell--short' >${album.id}</div>
-        <div class='table__cell table__cell'>${album.title}</div>
+        <div class='table__row album__row'
+            id="${album.id}"
+            data-value="${album}" draggable="true" ondragstart="drag(event)">
+            <div class='table__cell table__cell--short' >${album.id}</div>
+            <div class='table__cell table__cell'>${album.title}</div>
         </div>
         `);
     });
@@ -91,57 +82,42 @@ const swapAlbum = async (receivingUserId, albumId, direction) => {
   try {
     const album = await fetch(`${url}/${albumId}`, {
       method: 'PATCH',
-      body: JSON.stringify({
-        userId: receivingUserId,
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
+      body: JSON.stringify({userId: receivingUserId}),
+      headers: {'Content-type': 'application/json; charset=UTF-8'}
     });
-    const json = await album.json();
-    return json;
+    return await album.json();
   } catch (error) {
     return error;
   }
 };
 
-let dragArr = new Set();
+let multiSelect = new Set();
 
-function allowDrop(ev) {
-  ev.preventDefault();
+const allowDrop = ev => ev.preventDefault();
+
+const drag = (ev) =>{
+  ev.dataTransfer.setData('text', ev.target.id);
+  const originTableId = $(ev.target).parent().attr('id');
+  const dropZone = originTableId === 'table-2' ? '#table-1' : '#table-2';
+
+  $(dropZone).addClass('drop__zone');
 }
 
-function drag(ev) {
-  ev.dataTransfer.setData('text', ev.target.id, 0);
-
-  const dragParentId = $(ev.target)
-    .parent()
-    .attr('id');
-  const otherTable = dragParentId === 'table-2' ? '#table-1' : '#table-2';
-  $(otherTable).addClass('drop__zone');
-  ev.dataTransfer.setData('green-border', otherTable, 1);
-}
-
-async function drop(ev) {
+const drop = async ev => {
   ev.preventDefault();
   let data = ev.dataTransfer.getData('text');
   let dropZone = ev.currentTarget;
   let userId = dropZone.getAttribute('data-value');
 
-  [...dragArr].forEach(async albumId => {
-      const droppedAlbum = await swapAlbum(userId, albumId);
-      const idToFind = String(droppedAlbum.id);
-      console.log(droppedAlbum);
-      //idToFind is the New Owners ID
-      dropZone.appendChild(document.getElementById(idToFind));
-    });
-    droppedAlbum = await swapAlbum(userId, data);
-    idToFind = String(droppedAlbum.id);
-    dropZone.appendChild(document.getElementById(idToFind));
+  [...multiSelect].map(async albumId => {
+    const droppedAlbum = await swapAlbum(userId, albumId);
+    dropZone.appendChild(document.getElementById(droppedAlbum.id));
+  });
+  droppedAlbum = await swapAlbum(userId, data);
+  dropZone.appendChild(document.getElementById(droppedAlbum.id));
 
-  const thisTable = dropZone.id === 'table-1' ? '#table-1' : '#table-2';
-  $(thisTable).removeClass('drop__zone');
-  dragArr = new Set()
-  $(`#${idToFind}`).addClass('selected')
+  $('.table').removeClass('drop__zone');
+  multiSelect = new Set();
+  $(`#${droppedAlbum.id}`).addClass('selected');
   setTimeout(() => $('.album__row').removeClass('selected'), 600);
 }
